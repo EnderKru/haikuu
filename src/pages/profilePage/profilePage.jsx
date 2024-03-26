96f6c8317f2053f738d976107c8917b6d1ae1f71
EnderKru <enderpro700@gmail.com>
2024-03-26 12:49:05 +0300

    profile page

--- src/pages/profilePage/profilePage.jsx
+++ src/pages/profilePage/profilePage.jsx
@@ -1,5 +1,59 @@
+import classes from './profilePage.module.css'
+
 export function ProfilePage(){
+    function editProfile(){
+        alert("You cant edit it now. sorry")
+    }
+    function dontWork(){
+        alert("this also doesn't work.")
+    }
     return(
-        <></>
+        <div className={classes.profBody}>
+            <div className={classes.conteiner}>
+                <div className={classes.user}>
+                    <div className={classes.userInfo}>
+                        <img src="../src/assets/фото/image 8person.png" alt="Mini Photo" />
+                        <div className={classes.userName}>
+                            <div className={classes.userNickname}>NickName</div>
+                        </div>
+                    </div>
+                    <button className={classes.userEdit} onClick={editProfile}>edit profile</button>
+                    
+                </div>
+                <div className={classes.personalData}>
+                    <div className={classes.parsDataTask}>Personal data </div>
+                    <div className={classes.personalInfo}>
+                    <div className={classes.emailBlock}>
+                        <div className={classes.leftEmail}>
+                            <div className={classes.email}>
+                                <img src='../src/assets/images/email.svg'/>
+                                <div className={classes.emailInfo}>
+                                    <div className={classes.task}>email</div>
+                                    <div className={classes.sub}>em***@gmail.com</div>
+                                </div>
+                            </div>
+                        </div>
+                        <div className={classes.rightEmail}>
+                            <button className={classes.change} onClick={dontWork}>change</button>
+                        </div>
+                    </div>
+                    <div className={classes.passwordBlock}>
+                        <div className={classes.leftPassword}>
+                            <div className={classes.password}>
+                            <img src='../src/assets/images/key.svg'/>
+                            <div className={classes.passwordInfo}>
+                                <div className={classes.task}>password</div>
+                                <div className={classes.sub}>**********</div>
+                            </div>
+                            </div> 
+                        </div>
+                        <div className={classes.rightPassword}>
+                            <button className={classes.change} onClick={dontWork}>change</button>
+                        </div>
+                    </div>
+                    </div>
+                </div>
+            </div>
+        </div>
     )
 }
